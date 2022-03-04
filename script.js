8987645554e438d3c88ddc9a68b362d34e8fad9c
GustavoJuvino <juvinogustavo1@gmail.com>
2022-03-04 09:53:29 -0300

    Amazon Clone

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-// document.getElementsByClassName(".showcase").style.visibility = "hidden"
+// Scroll Suave
+
+function smoothScroll() {
+const internLinks = document.querySelectorAll('.js-scroll a[href^="#"]');
+
+function scrollToSection(event) {
+    event.preventDefault();
+    const href= event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+    });
 
 
-document.getElementById('.showcase').style.display = "none";
+
+// Alternative Form
+    // const top = section.offsetTop;
+    // window.scrollTo({
+    //     top: top,
+    //     behavior: 'smooth',
+    // })
+}
+
+
+internLinks.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+})
+}
+smoothScroll();
