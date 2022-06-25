18bed76da41f1c668123b7e7db1cabb3b4b7c46e
グスタボGustavo <juvinogustavo1@gmail.com>
2022-06-25 09:32:32 -0300

    :recycle: JS file

--- script.js
+++ script.js
@@ -5,16 +5,15 @@ const internLinks = document.querySelectorAll('.js-scroll a[href^="#"]');
 
 function scrollToSection(event) {
     event.preventDefault();
-    const href= event.currentTarget.getAttribute('href');
+    const href = event.currentTarget.getAttribute("href");
     const section = document.querySelector(href);
     section.scrollIntoView({
         behavior: 'smooth',
         block: 'start',
     });
 }
-internLinks.forEach((link) => {
-    link.addEventListener('click', scrollToSection);
-})
+
+internLinks.forEach(link => link.addEventListener("click", scrollToSection));
     
 }
 smoothScroll();
@@ -22,18 +21,12 @@ smoothScroll();
 
 
 // Effect Hoover - Top Categories
-const aEffect = document.querySelector('.a-effect');
-const navMenu = document.querySelector('.categories-section');
+const aEffect = document.querySelector(".a-effect");
+const navMenu = document.querySelector(".categories-section");
 
 function hooverCategories() {
-    aEffect.addEventListener('mouseenter', () => {
-        navMenu.classList.toggle('hover-effect');
-    })
-    
-    navMenu.addEventListener('mouseleave', () => {
-        navMenu.classList.toggle('hover-effect');
-    })
-    
+    aEffect.addEventListener("mouseenter", () => navMenu.classList.toggle("hover-effect"));
+    navMenu.addEventListener("mouseleave", () => navMenu.classList.toggle("hover-effect"));
 }
 
 hooverCategories();
@@ -41,20 +34,20 @@ hooverCategories();
 
 
 // Effect Clcik - Languages Section
-const liEffect = document.querySelector('.l-acronyms');
-const languageMenu = document.querySelector('.languages-section');
-const selectMain = document.querySelector('#all');
-const shadowModal = document.querySelector('.modal-container');
+const liEffect = document.querySelector(".l-acronyms");
+const languageMenu = document.querySelector(".languages-section");
+const selectMain = document.querySelector("#all");
+const shadowModal = document.querySelector(".modal-container");
 
 
 function clickLanguage(){
-    liEffect.addEventListener('click', () => {
-        languageMenu.classList.toggle('hover-effect-language');
-        shadowModal.classList.toggle('modal-active');
+    liEffect.addEventListener("click", () => {
+        languageMenu.classList.toggle("hover-effect-language");
+        shadowModal.classList.toggle("modal-active");
     })
-    liEffect.addEventListener('touchstart', () => {
-        languageMenu.classList.toggle('hover-effect-language');
-        shadowModal.classList.toggle('modal-active');
+    liEffect.addEventListener("touchstart", () => {
+        languageMenu.classList.toggle("hover-effect-language");
+        shadowModal.classList.toggle("modal-active");
     })
 }
 clickLanguage();
@@ -62,18 +55,18 @@ clickLanguage();
 
 
 // Effect Click - Acc Section
-const accEffect = document.querySelector('.acc-effect');
-const accMenu = document.querySelector('.menu-acc')
+const accEffect = document.querySelector(".acc-effect");
+const accMenu = document.querySelector(".menu-acc")
 
 function accClick()  {
-    accEffect.addEventListener('click', () => {
-        accMenu.classList.toggle('menu-click-effect');
-        shadowModal.classList.toggle('modal-active');
+    accEffect.addEventListener("click", () => {
+        accMenu.classList.toggle("menu-click-effect");
+        shadowModal.classList.toggle("modal-active");
     })
 
-    accEffect.addEventListener('touchstart', () => {
-        accMenu.classList.toggle('menu-click-effect');
-        shadowModal.classList.toggle('modal-active');
+    accEffect.addEventListener("touchstart", () => {
+        accMenu.classList.toggle("menu-click-effect");
+        shadowModal.classList.toggle("modal-active");
     })
 }
 
@@ -100,11 +93,11 @@ checkLink();
 
 // Close Menus
 function closeMenus(){
-    selectMain.addEventListener('click', ()=>{
-        selectMain.classList.toggle('close')
-        languageMenu.classList.add('hover-effect-language');
-        accMenu.classList.add('menu-click-effect');
-        shadowModal.classList.add('modal-active');
+    selectMain.addEventListener("click", ()=>{
+        selectMain.classList.toggle("close")
+        languageMenu.classList.add("hover-effect-language");
+        accMenu.classList.add("menu-click-effect");
+        shadowModal.classList.add("modal-active");
     })
 }
 closeMenus();
